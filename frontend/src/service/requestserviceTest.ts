4ea3e3c9144fe6bf71872dbb4a0aab2cdd9a2018
Niklas Schibel <niklas.schibel@gmx.de>
2022-01-26 17:34:37 +0100

    basic funktionalität implementiert, User Listen, und Authentification

--- frontend/src/service/requestserviceTest.ts
+++ frontend/src/service/requestserviceTest.ts
@@ -1,48 +1,15 @@
 import axios from "axios";
 import {Item} from "../models/ItemModel";
 import {LoginData} from "../models/LoginData";
+import {useContext} from "react";
+import {AuthContext} from "../context/AuthProvider";
 
-// const INDEV = process.env.NODE_ENV === "development"
-// const BASEURL: string = INDEV ? "http://localhost:8080" : "";
-
-// const headers = token => ({
-//     headers: {
-//         Authorization: `Bearer ${token}`,
-//     },
-// })
-
-export const updateItem = (item: Item, token?: string) => {
-    axios.put(`/api/shoppinglist/test/Mongo/${item.id}`, item, token ? {
-        headers: {
-            "Authorization": "Bearer " + token
-        }
-    } : {}).catch(console.error)
-}
-
-//kann das im FrontEnd weg?
-export const getItemByID: (item: Item) => void =
-    (item) => {
-        axios.get(`/api/shoppinglist/test/Mongo/${item.id}`).catch(console.error)
-    }
-
-export const deleteItemByID = (item: Item) =>
-    axios.delete(`/api/shoppinglist/test/Mongo/${item.id}`)
-        .then(() => console.log("delete Successfull"))
-        .catch((error) => {
-            console.log(error.message);
-        })
 
 export const loginRequest = (login: LoginData) =>
     axios.post(`/auth/login`, login)
         .then(response => response.data)
         .catch(console.error)
 
-
-//vielleicht noch mitverwenden
-
-// + loca
-// du kannst später aus dem TOken den Username extrahieren!!
-//Todo passt das mit token?{headers... Minute 21:54 im Video
 
 export const getAllTodosTest = (token?: string) =>
 
@@ -55,4 +22,29 @@ export const getAllTodosTest = (token?: string) =>
             console.log(error);
         })
 
-// export const deleteTodo = id => axios.delete(`/api/todo/${id}`)
+export const updateItem = (item: Item, token?: string) => {
+    axios.put(`/api/shoppinglist/test/Mongo/${item.id}`, item, token ? {
+        headers: {
+            "Authorization": "Bearer " + token
+        }
+    } : {}).catch(console.error)
+}
+
+//todo delete funktioniert noch nicht
+export const deleteItemByID = (item: Item, token?: string) =>
+    axios.delete(`/api/shoppinglist/test/Mongo/${item.id}`, token ? {
+        headers: {
+            "Authorization": "Bearer " + token
+        }
+    } : {})
+        .then(() => console.log("Delete was Successfull"))
+        .catch((error) => {
+            console.log(error.message);
+        })
+
+//kann das im FrontEnd weg?
+// export const getItemByID: (item: Item) => void =
+//     (item) => {
+//         axios.get(`/api/shoppinglist/test/Mongo/${item.id}`).catch(console.error)
+//     }
+
